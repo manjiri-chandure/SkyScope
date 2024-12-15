310b59da46c9c6ccbf44d23e2309b886a52b7610
manjiri <chanduremanjiri@gmail.com>
2024-12-15 22:05:27 +0530

    initial commit for RestAPI

---
+++ RestAPI/skyscope/src/main/java/com/example/skyscope/SkyscopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyscopeApplication.class, args);
+	}
+
+}
---
+++ RestAPI/skyscope/src/test/java/com/example/skyscope/SkyscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
